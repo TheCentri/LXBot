fe7641645c1725b5502ccd4c60ab4fd43dcc6209
TheCentri <nile318@gmail.com>
2016-11-29 09:34:00 -0500

    Added weather command

--- lxbot.js
+++ lxbot.js
@@ -1,6 +1,7 @@
 const config =  require('./config/config.json');
 const response = require('./responses/response.json');
 const fs = require('fs');
+const request = require('request');
 const timeStamp = require('time-stamp');
 const eris = require('eris');
 
@@ -20,7 +21,7 @@ if(message.content.startsWith(prefix+"say")){bot.createMessage(message.channel.i
 
 if(message.content.startsWith(prefix+"coinFlip")){
   let flipTimes = message.content.split(" ").slice(1).join(" ");
-  if(isNaN(flipTimes)){bot.createMessage(message.channel.id, "I need a number");return;} 
+  if(isNaN(flipTimes)){bot.createMessage(message.channel.id, "I need a number");return;}
   if (flipTimes > 10 || flipTimes < 1){bot.createMessage(message.channel.id, "Please use a number between 1 and 10");return;}
   if(flipTimes.length == 0){
     let math = Math.floor((Math.random() * 10) + 1);
@@ -45,6 +46,28 @@ if(message.content.startsWith(prefix+"coinFlip")){
     bot.createMessage(message.channel.id, result + `\nTotal\nHeads: ${a}\nTails: ${b}`);
     }
   }
+
+  if(message.content.startsWith(prefix+"weather")){
+    let address = message.content.split(" ").slice(1).join("+");
+    let geocodeURL= `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${config.geocodeToken}`
+    request(geocodeURL, function(error,response,body){
+      if(!error && response.statusCode == 200){
+         let arg = JSON.parse(body);
+         if(arg.status == "ZERO_RESULTS" ){bot.createMessage(message.channel.id,"I could not find the location you entered");return;}
+         let lat = arg.results[0].geometry.location.lat;
+         let lon = arg.results[0].geometry.location.lng;
+         let weatherURL = `https://api.darksky.net/forecast/${config.forecastIO}/${lat},${lon}`
+         request(weatherURL, function(error,response,body) {
+           if(!error && response.statusCode == 200){
+             let forecast = JSON.parse(body);
+             bot.createMessage(message.channel.id,
+               `**Weather for today in  ${arg.results[0].address_components[1].long_name}:** ${forecast.hourly.data[0].summary}\n\n`+
+               `Tempature: ${forecast.hourly.data[0].apparentTemperature} F\n`+
+               `Humidity: ${forecast.hourly.data[0].humidity}\n`+
+               `Wind Speed: ${forecast.hourly.data[0].windSpeed} MPH\n`+
+               `Visibility: ${forecast.hourly.data[0].visibility} Miles`)
+    }});}})};
+
 });
 
 bot.connect();
