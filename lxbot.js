f6d3b9e6e2327bcb3ea0ac519bce8435ce083d08
Brandon <nile318@gmail.com>
2016-11-30 01:22:06 -0500

    Started adding modularity

--- lxbot.js
+++ lxbot.js
@@ -1,46 +1,84 @@
 const config =  require('./config/config.json');
 const response = require('./responses/response.json');
+const commands = require('./commands/commands.js');
 const fs = require('fs');
 const request = require('request');
 const timeStamp = require('time-stamp');
-const eris = require('eris');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-
-var bot = new eris(config.token);
 
 bot.on('ready',() =>{
 console.log("Ready");
 });
 
-bot.on('messageCreate', (message) =>{
-if(message.author.bot)return;
-var prefix = "~";
-var input = message.content.toLowerCase();
-var command = message.content.split(" ");
-command = command[0];
-console.log(command);
-if(response[input]){bot.createMessage(message.channel.id, response[input])}
-if(message.content.startsWith(prefix+"say")){bot.createMessage(message.channel.id, message.content.split(" ").slice(1).join(" "));}
+bot.on('message', message => {
+  if(message.author.bot)return;
+  var prefix = "~";
+  var input = message.content.toLowerCase();
+  var command = message.content.split(" ");
+  command = command[0];
+  let cmd = commands[command];
 
-if(command.toLowerCase() == (prefix+"addcomm")){
-  let command = message.content.split(" ").slice(1);
-  let key = JSON.stringify(prefix+command[0]);
-  console.log(key);
-  let value = JSON.stringify(message.content.split(" ").slice(2).join(" "));;
-  let newCommand = key.toLowerCase()+":"+value;
-  let a = JSON.stringify(response)
-  a = a.substring(0, a.length - 1);
-  let file = fs.createWriteStream('./responses/response.json');
-  file.write(a+","+newCommand+`\n}`);
+  if(cmd){
+    cmd.process(bot, message, command);
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*if(response[input]){message.channel.sendMessage(response[input])}
+  if(message.content.startsWith(prefix+"say")){message.channel.sendMessage(message.content.split(" ").slice(1).join(" "));}
+
+  if(command.toLowerCase() == (prefix + "voice")){
+    let voiceChannel = message.member.voiceChannel;
+    voiceChannel.join();
+
+  }
+
+  if(command.toLowerCase == (prefix+"alias")){
+    let command = message.content.split(" ").slice(1);
+    let key = JSON.stringify(prefix+command[0]);
+    let value = JSON.stringify(message.content.split(" ").slice(2).join(" "));;
+    let newCommand = key.toLowerCase()+":"+value;
+    let a = JSON.stringify(response)
+    a = a.substring(0, a.length - 1);
+    let file = fs.createWriteStream('./responses/response.json');
+    file.write(a+","+newCommand+`\n}`);
 }
 
 if(command.toLowerCase() == (prefix+"coinflip")){
   let flipTimes = message.content.split(" ").slice(1).join(" ");
-  if(isNaN(flipTimes)){bot.createMessage(message.channel.id, "I need a number");return;}
-  if (flipTimes > 10 || flipTimes < 1){bot.createMessage(message.channel.id, "Please use a number between 1 and 10");return;}
+  if(isNaN(flipTimes)){message.channel.sendMessage( "I need a number");return;}
+  if (flipTimes > 10 || flipTimes < 1){message.channel.sendMessage( "Please use a number between 1 and 10");return;}
   if(flipTimes.length == 0){
     let math = Math.floor((Math.random() * 10) + 1);
-    if(math >= 5){bot.createMessage(message.channel.id, "Heads");} else{bot.createMessage(message.channel.id,"Tails");return;}
+    if(math >= 5){message.channel.sendMessage( "Heads");} else{message.channel.sendMessage("Tails");return;}
   }else{
     let i = 1;
     let result = `Results of the ${flipTimes} coin flips\n\n`;
@@ -58,7 +96,7 @@ if(command.toLowerCase() == (prefix+"coinflip")){
 
         result += `${i}) ${coinResult}\n`
       }
-    bot.createMessage(message.channel.id, result + `\nTotal\nHeads: ${a}\nTails: ${b}`);
+    message.channel.sendMessage( result + `\nTotal\nHeads: ${a}\nTails: ${b}`);
     }
   }
 
@@ -68,53 +106,51 @@ if(command.toLowerCase() == (prefix+"coinflip")){
     request(geocodeURL, function(error,response,body){
       if(!error && response.statusCode == 200){
          let arg = JSON.parse(body);
-         if(arg.status == "ZERO_RESULTS" ){bot.createMessage(message.channel.id,"I could not find the location you entered");return;}
+         if(arg.status == "ZERO_RESULTS" ){message.channel.sendMessage("I could not find the location you entered");return;}
          let lat = arg.results[0].geometry.location.lat;
          let lon = arg.results[0].geometry.location.lng;
          let weatherURL = `https://api.darksky.net/forecast/${config.forecastIO}/${lat},${lon}`
          request(weatherURL, function(error,response,body) {
            if(!error && response.statusCode == 200){
              let forecast = JSON.parse(body);
-             bot.createMessage(message.channel.id,
+             message.channel.sendMessage(
                `**Weather for today in  ${arg.results[0].address_components[1].long_name}:** ${forecast.hourly.data[0].summary}\n\n`+
                `Tempature: ${forecast.hourly.data[0].apparentTemperature} F\n`+
                `Humidity: ${forecast.hourly.data[0].humidity}\n`+
-               `Wind Speed: ${forecast.hourly.data[0].windSpeed} MPH\n`+
+               `Wind Speed: ${forecast.hourly.data[0].windSpeed} Mph\n`+
                `Visibility: ${forecast.hourly.data[0].visibility} Miles`)
     }});}})};
-    if(command.toLowerCase() == (prefix + "imdb")){
-      let media = message.content.split(" ").slice(1).join("+");
-      let imbdURL = `http://www.omdbapi.com/?t=${media}&y=&plot=full&r=json`;
-      request(imbdURL, function(error,response,body){
+  if(command.toLowerCase() == (prefix + "imdb")){
+    let media = message.content.split(" ").slice(1).join("+");
+    let imbdURL = `http://www.omdbapi.com/?t=${media}&y=&plot=full&r=json`;
+    request(imbdURL, function(error,response,body){
         if(!error && response.statusCode == 200){
           let obj = JSON.parse(body);
-          if(obj.Title == undefined){bot.createMessage(message.channel.id,"Unknown Movie or Show");return;}
-          bot.createMessage(message.channel.id,`**IMDB Info**\n\n**Title:** ${obj.Title}\n**Year:** ${obj.Year}\n**Rating:** ${obj.Rated}\n**Plot:** ${obj.Plot}`);
+          if(obj.Title == undefined){message.channel.sendMessage("Unknown Movie or Show");return;}
+          message.channel.sendMessage(`**IMDB Info**\n\n**Title:** ${obj.Title}\n**Year:** ${obj.Year}\n**Rating:** ${obj.Rated}\n**Plot:** ${obj.Plot}`);
         }});}
 
 
-             if(command.toLowerCase() == (prefix + "searchimdb")){
-                  let media = message.content.split(" ").slice(1).join("+");
-                  if (media.length === 0){bot.createMessage(message.channel.id,"I need search parameters");return;}
-                  let imbdURL = `http://www.omdbapi.com/?s=${media}&y=&plot=full&r=json`;
-                  request(imbdURL, function(error,response,body){
-                    if(!error && response.statusCode == 200){
-                      let obj = JSON.parse(body);
-                      let results = `**Showing top results from ${obj.totalResults} Results**\n\n`
-                      let i = 1;
-                      for(i in obj.Search){
-                        results += `**${parseInt(i)+1}):**  ${obj.Search[i].Title} *(${obj.Search[i].Year}) IMDB ID:  ${obj.Search[i].imdbID}*\n`;
-                        i++
-                      }
-                      bot.createMessage(message.channel.id,results);
-                    }else{
-                      bot.createMessage(message.channel.id,"No results found :(");
-                    }
-              });
-
-            }
-});
+  if(command.toLowerCase() == (prefix + "searchimdb")){
+    let media = message.content.split(" ").slice(1).join("+");
+    if (media.length === 0){message.channel.sendMessage("I need search parameters");return;}
+    let imbdURL = `http://www.omdbapi.com/?s=${media}&y=&plot=full&r=json`;
+    request(imbdURL, function(error,response,body){
+      if(!error && response.statusCode == 200){
+        let obj = JSON.parse(body);
+        let results = `**Showing top results from ${obj.totalResults} Results**\n\n`
+        let i = 1;
+        for(i in obj.Search){
+          results += `**${parseInt(i)+1}):**  ${obj.Search[i].Title} *(${obj.Search[i].Year}) IMDB ID:  ${obj.Search[i].imdbID}*\n`;
+          i++
+        }
+        message.channel.sendMessage(results);
+        }else{message.channel.sendMessage("No results found :(");
+        }
+      });
+}*/
+//});
 
 
 
-bot.connect();
+bot.login(config.token);
