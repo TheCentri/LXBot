0a8f33be207e80cb6db66a5c25dea3cdcc510230
TheCentri <nile318@gmail.com>
2016-11-29 21:45:19 -0500

    Added Search, all commands go through as lowercase, addcomm command is a little bit better

--- lxbot.js
+++ lxbot.js
@@ -16,21 +16,25 @@ bot.on('messageCreate', (message) =>{
 if(message.author.bot)return;
 var prefix = "~";
 var input = message.content.toLowerCase();
+var command = message.content.split(" ");
+command = command[0];
+console.log(command);
 if(response[input]){bot.createMessage(message.channel.id, response[input])}
 if(message.content.startsWith(prefix+"say")){bot.createMessage(message.channel.id, message.content.split(" ").slice(1).join(" "));}
 
-if(message.content.startsWith(prefix+"addcomm")){
+if(command.toLowerCase() == (prefix+"addcomm")){
   let command = message.content.split(" ").slice(1);
   let key = JSON.stringify(prefix+command[0]);
-  let value = JSON.stringify(message.content.split(" ").slice(2).join(" "));
-  let newCommand = key+":"+value;
+  console.log(key);
+  let value = JSON.stringify(message.content.split(" ").slice(2).join(" "));;
+  let newCommand = key.toLowerCase()+":"+value;
   let a = JSON.stringify(response)
   a = a.substring(0, a.length - 1);
   let file = fs.createWriteStream('./responses/response.json');
   file.write(a+","+newCommand+`\n}`);
 }
 
-if(input == (prefix+"coinflip")){
+if(command.toLowerCase() == (prefix+"coinflip")){
   let flipTimes = message.content.split(" ").slice(1).join(" ");
   if(isNaN(flipTimes)){bot.createMessage(message.channel.id, "I need a number");return;}
   if (flipTimes > 10 || flipTimes < 1){bot.createMessage(message.channel.id, "Please use a number between 1 and 10");return;}
@@ -58,7 +62,7 @@ if(input == (prefix+"coinflip")){
     }
   }
 
-  if(message.content.startsWith(prefix+"weather")){
+  if(command.toLowerCase() == (prefix+"weather")){
     let address = message.content.split(" ").slice(1).join("+");
     let geocodeURL= `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${config.geocodeToken}`
     request(geocodeURL, function(error,response,body){
@@ -78,8 +82,39 @@ if(input == (prefix+"coinflip")){
                `Wind Speed: ${forecast.hourly.data[0].windSpeed} MPH\n`+
                `Visibility: ${forecast.hourly.data[0].visibility} Miles`)
     }});}})};
+    if(command.toLowerCase() == (prefix + "imdb")){
+      let media = message.content.split(" ").slice(1).join("+");
+      let imbdURL = `http://www.omdbapi.com/?t=${media}&y=&plot=full&r=json`;
+      request(imbdURL, function(error,response,body){
+        if(!error && response.statusCode == 200){
+          let obj = JSON.parse(body);
+          if(obj.Title == undefined){bot.createMessage(message.channel.id,"Unknown Movie or Show");return;}
+          bot.createMessage(message.channel.id,`**IMDB Info**\n\n**Title:** ${obj.Title}\n**Year:** ${obj.Year}\n**Rating:** ${obj.Rated}\n**Plot:** ${obj.Plot}`);
+        }});}
 
+
+             if(command.toLowerCase() == (prefix + "searchimdb")){
+                  let media = message.content.split(" ").slice(1).join("+");
+                  if (media.length === 0){bot.createMessage(message.channel.id,"I need search parameters");return;}
+                  let imbdURL = `http://www.omdbapi.com/?s=${media}&y=&plot=full&r=json`;
+                  request(imbdURL, function(error,response,body){
+                    if(!error && response.statusCode == 200){
+                      let obj = JSON.parse(body);
+                      let results = `**Showing top results from ${obj.totalResults} Results**\n\n`
+                      let i = 1;
+                      for(i in obj.Search){
+                        results += `**${parseInt(i)+1}):**  ${obj.Search[i].Title} *(${obj.Search[i].Year}) IMDB ID:  ${obj.Search[i].imdbID}*\n`;
+                        i++
+                      }
+                      bot.createMessage(message.channel.id,results);
+                    }else{
+                      bot.createMessage(message.channel.id,"No results found :(");
+                    }
+              });
+
+            }
 });
+
 
 
 bot.connect();
