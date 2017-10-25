9a8092d296796033a585c0dd76b6ee2a468695d6
Brandon Nile <Brandon@ad.bnile.xyz>
2017-10-25 12:00:26 -0700

    Updates

--- lxbot.js
+++ lxbot.js
@@ -5,6 +5,50 @@ const response = require('./responses/response.json');
 const Discord = require('discord.js');
 const timeStamp = require('time-stamp');
 const ytdl = require('ytdl-core');
+
+
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host : "192.168.0.160",
+  user : "lxbot",
+  password : config.lxbot
+});
+
+var pool      =    mysql.createPool({
+  connectionLimit : 100, //important
+  host     : '192.168.0.160',
+  user     : 'lxbot',
+  password : config.lxbot,
+  database : 'LXBot',
+  debug    :  false
+});
+
+
+function handle_database(req,res) {
+  
+  pool.getConnection(function(err,connection){
+      if (err) {
+        res.json({"code" : 100, "status" : "Error in connection database"});
+        return;
+      }   
+
+      console.log('connected as id ' + connection.threadId);
+      
+      connection.query("select * from user",function(err,rows){
+          connection.release();
+          if(!err) {
+              res.json(rows);
+          }           
+      });
+
+      connection.on('error', function(err) {      
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;     
+      });
+});
+}
+
 const bot = new Discord.Client();
 var prefix = "~";
 
@@ -13,9 +57,7 @@ bot.on('ready', () => {
 });
 bot.on('message', message => {
   if (message.author.bot) return;
-  if (!message.content.startsWith('~')) {
-    return;
-  }
+  if (!message.content.startsWith('~')) return;
   let input = message.content.split(" ");
   let command = message.content.slice(1).split(" ");
   command = command[0].toLowerCase();
